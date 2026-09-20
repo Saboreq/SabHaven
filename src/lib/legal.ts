8dd7ceb5580c64dacb1a2dd02d6ff9f63d98267c
Saboreq <118023809+Saboreq@users.noreply.github.com>
2026-09-20 14:02:24 +0200

    feat: add legal and account safety framework

---
+++ src/lib/legal.ts
@@ -0,0 +1,6 @@
+export const LEGAL_OPERATOR = 'Saboreq';
+export const LEGAL_CONTACT_EMAIL = 'contact@saboreq.xyz';
+export const TERMS_VERSION = '2026-09-20';
+export const PRIVACY_VERSION = '2026-09-20';
+export const HOSTED_SERVICE_URL = 'https://files.saboreq.xyz';
+export const GITHUB_REPOSITORY_URL = 'https://github.com/Saboreq/SabHaven';
